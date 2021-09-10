021ffe8b7a3fce42d6f0f95cb109b599030227be
B-E-C-H <mikkel.l.bech@gmail.com>
2021-09-10 13:43:52 +0200

    Iterator Excersize

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,8 +1,28 @@
+using System.Collections.Generic;
+using System;
 using Xunit;
 
-namespace Assignment1.Tests
-{
-    public class IteratorsTests
-    {
+namespace Assignment1.Tests {
+
+    public class IteratorsTests {
+
+
+        [Fact]
+        public void FlattenReturnsStreamOfT() {
+
+            var testEnum = new List<List<int>> {
+                new List<int> { 0, 1, 2, 3, 4 },
+                new List<int> { 5, 6, 7, 8 },
+                new List<int> { 9, 10, 11, 12 }
+            };
+
+
+            var test = Assignment1.Iterators.Flatten<int>(testEnum);
+            var expected = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+            Assert.Equal(test, expected);
+
+        }
+
     }
 }
