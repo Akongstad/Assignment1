76df09ade465127e048534590f63745ea5598170
Joakim <jodi@itu.dk>
2021-09-10 13:52:09 +0200

    implemented and tested Iterators

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,8 +1,31 @@
 using Xunit;
+using System.Collections.Generic;
 
 namespace Assignment1.Tests
 {
+    
+
     public class IteratorsTests
     {
+
+        [Fact]
+
+        public void Flatten_returns_stream_of_T()
+        {
+            //Assign
+            var testStream = new List<List<int>> {
+                new List<int> {1,2,3,4,5},
+                new List<int> {11,22,53,74,85},
+                new List<int> {12,25,37,44,52}
+            };
+
+            var expected = new List<int> {1,2,3,4,5,11,22,53,74,85,12,25,37,44,52};
+
+            //Act
+            var actual = Assignment1.Iterators.Flatten<int>(testStream);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -7,12 +7,28 @@ namespace Assignment1
     {
         public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
         {
-            throw new NotImplementedException();
+            foreach (var item in items) {
+                foreach(var i in item) 
+                {
+                    yield return i;
+                }
+            }
         }
 
+        public static bool Even(int i) => i % 2 == 0;
+    
         public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
         {
-            throw new NotImplementedException();
+            foreach(var item in items){
+                if(predicate(item)){
+                    yield return item;
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Predicate<int> even = Even;
         }
     }
 }
