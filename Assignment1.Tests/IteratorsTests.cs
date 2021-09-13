e306d9da91ce0cf485294b09ac38f1c1b29ce6c5
Joakim <jodi@itu.dk>
2021-09-13 14:35:38 +0200

    added and tested Filter method

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using System.Collections.Generic;
+using System;
 
 namespace Assignment1.Tests
 {
@@ -23,6 +24,24 @@ namespace Assignment1.Tests
 
             //Act
             var actual = Assignment1.Iterators.Flatten<int>(testStream);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+
+        public void Filter_only_returns_even_number(){
+            
+            //Assign
+            Predicate<int> even = Assignment1.Iterators.Even;
+
+            var testStream = new List<int> {1,2,3,4,5,11,22,53,74,85,12,25,37,44,52};
+
+            var expected = new List<int> {2,4,22,74,12,44,52};
+
+            //Act
+            var actual = Assignment1.Iterators.Filter<int>(testStream, even);
 
             //Assert
             Assert.Equal(expected, actual);
