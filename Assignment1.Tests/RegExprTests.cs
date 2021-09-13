c2667617a4efe5ec3529029d0aceb4b442d01799
B-E-C-H <mikkel.l.bech@gmail.com>
2021-09-13 16:15:30 +0200

    Fixed SplitLine method + completed Resolutions

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -1,8 +1,29 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
 
-namespace Assignment1.Tests
-{
-    public class RegExprTests
-    {
+namespace Assignment1.Tests {
+    public class RegExprTests {
+
+        [Fact]
+        public void SplitLineIntoWords() {
+            var testArray = RegExpr.SplitLine(new List<string> { "awd dawdwa lolol", "diawd123 r432 lasd" });
+            var expected = new List<string> { "awd", "dawdwa", "lolol", "diawd123", "r432", "lasd" };
+
+            var actual = Assignment1.RegExpr.SplitLine(testArray);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void PrintResolutionPrLine() {
+            var testArray = new List<string> { "1920x1080", "1024x768, 800x600" };
+            var expected = new (int, int)[] { (1920, 1080), (1024, 768), (800, 600) };
+            var actual = Assignment1.RegExpr.Resolution(testArray);
+
+
+            Assert.Equal(expected, actual);
+
+        }
     }
 }
